428a7081cea238ea498df9769f11b8ef9e092421
Arrgrr <lenezgrand@gmail.com>
2019-07-05 17:53:09 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view class="header" name="header"></router-view>
+    <router-view class="container" name="search" v-if="isOpen= true"></router-view>
+    <router-view class="container"></router-view>
+    <router-view class="footer" name="footer"></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      isOpen: false
+    };
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "./src/globalStyle";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  flex: 1 100%;
+  height: 100vh;
+  margin: 0;
+}
+.header {
+  display: flex;
+  z-index: 1;
+}
+.container {
+  display: flex;
+  flex: 1;
+  height: 100%;
+  z-index: 0;
+}
+.footer {
+  display: flex;
 }
 </style>
